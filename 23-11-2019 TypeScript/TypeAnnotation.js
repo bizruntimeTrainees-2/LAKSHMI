5374d2b5a96776a518ef195d8206de40d43120f6
unknown <Lakshmiroyal.1997@gmail.com>
2019-11-23 18:00:30 +0530

    javascript

---
+++ 23-11-2019 TypeScript/TypeAnnotation.js
@@ -0,0 +1,16 @@
+var num1 = 1;
+function number() {
+    var num2 = 10;
+    if (num2 > num1) {
+        var num3 = 7;
+        num3++;
+    }
+    while (num1 < num2) {
+        var num4 = 10;
+    }
+    console.log(num2);
+    console.log(num3);
+    console.log(num4);
+}
+console.log(num1);
+number();
---
+++ 23-11-2019 TypeScript/TypeAnnotation.ts
@@ -0,0 +1,24 @@
+
+var num1:number = 1;
+
+
+function varNumber(){
+var num2: number= 10;
+
+  if(num2 > num1)
+  {
+    var num3:number = 7;
+    num3++;
+  }
+  while(num1 < num2)
+  {
+    var num4:number = 10;
+    
+  }
+  console.log(num1);
+  console.log(num2);
+  console.log(num3);
+   console.log(num4);
+}
+
+varNumber();
