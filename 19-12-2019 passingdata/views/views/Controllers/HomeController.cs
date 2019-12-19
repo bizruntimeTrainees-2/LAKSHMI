c9fbf8eccecf5fb47b8f29ffc3c771d33a06d6a2
unknown <Lakshmiroyal.1997@gmail.com>
2019-12-19 06:36:47 -0800

    ASP.NET

---
+++ 19-12-2019 passingdata/views/views/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using views.Models;
+using views.Viewmodels;
+
+namespace views.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+
+        }
+        public IActionResult Index()
+        {
+            return View("Views/Home/Index.cshtml");
+           
+        }
+
+       
+        
+        public ViewResult Details()
+        {
+
+            Employee model = _employeeRepository.GetEmployee(1);
+
+            //return View("views1/Home/Index.cshtml");
+            //return View(model); //this line is for Details.cshtml
+            //return View("MyViews/Test.cshtml");
+            //return View("../Test/Update");
+
+
+
+            // ViewData["Employee"] = model;
+            // ViewData["PageTitle"] = "Employee Details";
+            // return View();
+            ViewBag.Employee = model;
+            ViewBag.PageTitle = "EmployeeDetails";
+            return View();
+        }
+        public ViewResult IDetails1()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            ViewBag.PageTitle = "Employee Details";
+            return View(model);
+
+           
+        }
+        public ViewResult HomeDetails()
+        {
+           
+
+            HomeDetailsviewmodels homedetailsviewmodels = new HomeDetailsviewmodels()
+            {
+                Employee = _employeeRepository.GetEmployee(1),
+                 PageTitle = "Employee Details"
+                };
+            return View(homedetailsviewmodels);
+        
+        }
+
+        public ViewResult Index1()
+        {
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+
+        public ViewResult Index2()
+        {
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+
+        public ViewResult Details2()
+        {
+
+
+            HomeDetailsviewmodels homedetailsviewmodels = new HomeDetailsviewmodels()
+            {
+                Employee = _employeeRepository.GetEmployee(1),
+                PageTitle = "Employee Details"
+            };
+            return View(homedetailsviewmodels);
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ 19-12-2019 passingdata/views/views/Models/MockEmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace views.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            { new Employee() {Id = 1,Name = "Lakshmi",Department = "IT",Email = "lakky@gmail.com"},
+              new Employee() {Id = 1,Name = "priya",Department = "HR",Email = "priya@gmail.com"},
+              new Employee() {Id = 1,Name = "sunita",Department = "IT",Email = "suni@gmail.com"},
+             };
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
