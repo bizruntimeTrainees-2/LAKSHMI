c3c98d1b290d0b658d157da43e0453db8df07e4e
unknown <Lakshmiroyal.1997@gmail.com>
2019-12-21 04:19:47 -0800

    ASP.NET

---
+++ 21-12-2019 viewstart,viewimport , tagelpers/views/views/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using views.Models;
+using views.Viewmodels;
+
+namespace views.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+
+        }
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+            //return _employeeRepository.GetEmployee(1).Name;
+        }
+       
+      [Route("Home/Details")]
+        public ViewResult Details(int id)
+        {
+
+            // Employee model = _employeeRepository.GetEmployee(1);
+
+
+            // ViewData["Employee"] = model;
+            //ViewData["PageTitle"] = "Employee Details";
+            //return View();
+
+            /*
+             Employee model = _employeeRepository.GetEmployee(1);
+
+             ViewBag.Employee = model;
+              ViewBag.PageTitle = "EmployeeDetails";
+             return View();
+           */
+            //Employee model = _employeeRepository.GetEmployee(1);
+
+            //  ViewBag.PageTitle = "EmployeeDetails";
+            // return View(model); //this is for strongly typed
+
+            HomeDetailsviewmodels homeDetailsviewmodel = new HomeDetailsviewmodels()
+            {
+                Employee = _employeeRepository.GetEmployee(1),
+                //for putting id optional, then getEmployee(1) we wrote in place of id
+                PageTitle = "Employee Details"
+            };
+            return View(homeDetailsviewmodel);
+
+    }
+
+
+}
+}
---
+++ 21-12-2019 viewstart,viewimport , tagelpers/views/views/Viewmodels/HomeDetailsviewmodels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using views.Models;
+
+namespace views.Viewmodels
+{
+    public class HomeDetailsviewmodels
+    {
+        public Employee Employee { get; set; }
+        public string PageTitle { get; set; }
+
+    }
+}
